6e55510c24d058da31a87263cfe441da16648545
eugen-elanross <eugen@elanross.com>
2012-08-03 21:49:17 +0300

    making sandbox directory based (to avoid multiple repos)

---
+++ stellaSelenium/src/test/java/com/macys/stella/util/ContextConstants.java
@@ -0,0 +1,13 @@
+package com.macys.stella.util;
+
+public final class ContextConstants{
+	public static final String SUPER_USER = "SuperBAUser";
+	public static final String QA_USER = "QAUser";
+	public static final String VENDOR_USER = "VendorUser";
+	public static final String PASSWORD = "haht";
+	
+	private ContextConstants(){
+		throw new AssertionError();
+	}
+	
+}
