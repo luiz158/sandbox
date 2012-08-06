91de39ba6ea70ada2874fe00d6a5917520f44e98
eugenp <hanriseldon@gmail.com>
2012-08-06 15:32:11 +0300

    cleanup work

--- cardSample/README.txt
+++ cardSample/README.txt
@@ -1,4 +1,9 @@
 
 Development notes and decisions: 
+
 - the application is composed out of 2 deployable wars - the RESTful service and the client application (the UI)
-- 
+
+- to drive the REST work, I used two existing help jars - I am including the code for these as well: 
+-- rest-common - some base classes useful to quickly bootstrap a RESTful project; focus on some of the RESTful constraints - discoverability/HATEOAS, query support, etc
+-- rest-test - base test classes meant to ensure the correctness of the service
+
--- cardSample/card/src/test/java/org/rest/sec/persistence/service/BusinessCardServicePersistenceIntegrationTest.java
+++ cardSample/card/src/test/java/org/rest/sec/persistence/service/BusinessCardServicePersistenceIntegrationTest.java
@@ -1,17 +1,13 @@
 package org.rest.sec.persistence.service;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
-import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 import org.rest.common.persistence.service.IService;
 import org.rest.sec.model.BusinessCard;
-import org.rest.sec.model.ClientCard;
 import org.rest.sec.test.SecPersistenceServiceIntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-
-import com.google.common.collect.Sets;
 
 public class BusinessCardServicePersistenceIntegrationTest extends SecPersistenceServiceIntegrationTest<BusinessCard> {
 
@@ -34,23 +30,6 @@ public class BusinessCardServicePersistenceIntegrationTest extends SecPersistenc
 
         bService.create(createNewEntity(name));
         bService.create(createNewEntity(name));
-    }
-
-    // scenarios (in progress)
-
-    @Test
-    public void when_thenNoExceptions() {
-        final BusinessCard businessCard = bService.create(createNewEntity(randomAlphabetic(8)));
-        final ClientCard clientCard = cService.create(new ClientCard(randomAlphabetic(8)));
-
-        businessCard.setClientCards(Sets.newHashSet(clientCard));
-        bService.update(businessCard);
-
-        final BusinessCard updatedBusinessCard = bService.findOne(businessCard.getId());
-        final ClientCard updatedClientCard = cService.findOne(clientCard.getId());
-
-        assertFalse(updatedBusinessCard.getClientCards().isEmpty());
-        assertFalse(updatedClientCard.getBusinessCards().isEmpty());
     }
 
     // template method
